23b11c7088d2a423c8d97cd6683dcc5f93d0b0d0
vital <muvitaly5@gmail.com>
2023-04-15 20:31:24 -0500

    kuba made changes

--- src/main/java/GitAddCommit.java
+++ src/main/java/GitAddCommit.java
@@ -1,6 +1,7 @@
 public class GitAddCommit {
     public static void main(String[] args) {
-        System.out.println("First modification");
+        System.out.println("First modification");\
+        System.out.println("Added from Kuba ");
     }
 
 }
