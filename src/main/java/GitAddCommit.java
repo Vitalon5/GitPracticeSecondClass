016e3a0c38f88164cb90e9002e24c67af919cf3f
vital <muvitaly5@gmail.com>
2023-04-15 20:35:06 -0500

    new line after Kuba class

--- src/main/java/GitAddCommit.java
+++ src/main/java/GitAddCommit.java
@@ -1,7 +1,8 @@
 public class GitAddCommit {
     public static void main(String[] args) {
-        System.out.println("First modification");\
+        System.out.println("First modification");
         System.out.println("Added from Kuba ");
+        System.out.println("Modified files after kuba add another class");
     }
 
 }
